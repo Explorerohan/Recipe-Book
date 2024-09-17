d923ee5f85226600f931754a9177c1b30ab1834a
Rohan Rai <141544950+Explorerohan@users.noreply.github.com>
2024-09-17 22:52:30 +0545

    Update views.py

--- RecipeBook/Recipes/views.py
+++ RecipeBook/Recipes/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate,login, logout
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Creating my views here.
 @login_required(login_url='/')
 def add_recipes(request):
     if request.method == 'POST':
